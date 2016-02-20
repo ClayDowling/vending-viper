9d1d08bfe33e0f2292c96acd1e54bdd1ce0c3499
Clay Dowling <clay@lazarusid.com>
2016-02-20 13:36:12 -0500

    Added tool for manually setting pin state

---
+++ tools/set-pin.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+import argparse
+import RPi.GPIO as GPIO
+
+def doIt(pin, direction):
+
+    GPIO.setup(pin, GPIO.OUT)
+
+    GPIO.output(pin, direction)
+
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(description='Control LEDs')
+    parser.add_argument('pin', metavar='N', type=int, nargs='+', help='Pins to set')
+    parser.add_argument('--low', action='store_true', help='Set pins low (default)')
+    parser.add_argument('--high', action='store_true', help='Set pins high')
+
+    args = parser.parse_args()
+
+    GPIO.setmode(GPIO.BCM)
+
+    pinState = False
+    if args.high:
+        pinState = True
+
+    for pin in args.pin:
+        doIt(pin, pinState)
